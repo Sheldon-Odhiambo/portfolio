758ef0359143c1782746cbd2b170e280b9bfa89c
Sheldon Odhiambo <sheldonodhiuma04@gmail.com>
2023-08-02 12:57:40 +0300

    feat

--- script.js
+++ script.js
@@ -29,4 +29,32 @@ window.onscroll = () => {
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    // remove toggle icon and navbar when click navbar link (scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 };
+
+
+// scroll reveal
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img,.portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p,.skill-box', { origin: 'right' });
+
+// typed js
+
+const typed = new Typed ('.multiple-text',{
+    strings: ['Frontend Developer', 'Web Designer', 'Basketball Athlete'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 100,
+    loop: true
+})
